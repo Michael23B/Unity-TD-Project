a689b0b80498a1b27a368343501433b25f0f1bc3
yung lean <33080132+Michael23B@users.noreply.github.com>
2018-01-03 16:13:13 +1000

    changed script folder structure
    and started work on garden nodes

---
+++ Assets/Scripts/UI + Environment/GardenNode.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GardenNode : MonoBehaviour {
+    public Vector3 positionOffset;
+    public Color hoverColor;
+    public Color hoverColorBad;
+    [HideInInspector]
+    public GameObject plant;
+    [HideInInspector]
+    public TurretBlueprint plantBlueprint; //change to plant blueprint
+    [HideInInspector]
+    public bool isRipe = false;
+    [HideInInspector]
+    public float timeTillRipe = 0f;
+
+    //public GardenNodeUI; //(set in inspector)
+
+    private Color startColor;
+    private Renderer rend;
+
+    PlayerStats playerStats;
+
+    [HideInInspector]
+    public int nodeID;
+
+    LocalPlayerCommands player;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+        playerStats = PlayerStats.Instance;
+    }
+
+    void SelectGardenNode()
+    {
+        //GardenNodeUI.transform.position = transform.position + positionOffset;
+        //if its active, show() it else hide() it
+    }
+
+    private void OnMouseDown()
+    {
+        if (!WaveSpawner.Instance.buildTime) return;
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+
+        if (plant != null)
+        {
+            SelectGardenNode();
+            return;
+        }
+
+
+        //gardennodeUI has buttons to plant each plant which call CallPlantPlant(Plantblueprint plant); 
+    }
+
+    private void OnMouseEnter()
+    {
+        if (EventSystem.current.IsPointerOverGameObject()) return;
+        if (plant == null) return;
+
+        if (plant != null && !isRipe) rend.material.color = hoverColorBad;
+        else rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
---
+++ Assets/Scripts/UI + Environment/GardenNodes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GardenNodes : MonoBehaviour {
+
+    public static Transform[] gardenNodes;
+
+    private void Awake()
+    {
+        gardenNodes = new Transform[transform.childCount];
+        for (int i = 0; i < gardenNodes.Length; ++i)
+        {
+            gardenNodes[i] = transform.GetChild(i);
+            gardenNodes[i].GetComponent<GardenNode>().nodeID = i;
+        }
+    }
+}
